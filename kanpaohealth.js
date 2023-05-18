5f000db6d4a598f54e6f59b6b085256ca6ad709d
kanpaohealth2022 <133950166+kanpaohealth2022@users.noreply.github.com>
2023-05-18 14:04:07 +0700

    Create kanpaohealth.js

---
+++ kanpaohealth.js
@@ -0,0 +1,3 @@
+function setFooter(){
+  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/kanpaopublichealth" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Wansawang &copy; 2023 <a style="color:gray;text-decoration:none" href="https://kanpaohealth.blogspot.com" target="_blank"> Contact</a></center>'
+  }
