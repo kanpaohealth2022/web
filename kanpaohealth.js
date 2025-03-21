f3e6ae188df749ffc109c715f77435259408ddfb
kanpaohealth2022 <133950166+kanpaohealth2022@users.noreply.github.com>
2025-03-21 22:24:11 +0700

    Update kanpaohealth.js

--- kanpaohealth.js
+++ kanpaohealth.js
@@ -1,3 +1,3 @@
 function setFooter(){
-  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/kanpaopublichealth" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Wansawang &copy; 2023 <a style="color:gray;text-decoration:none" href="https://kanpaohealth.blogspot.com" target="_blank"> Contact</a></center>'
+  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/artwanreview" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Wansawang &copy; 2023 <a style="color:gray;text-decoration:none" href="https://www.wansawang.com" target="_blank"> Contact</a></center>'
   }
